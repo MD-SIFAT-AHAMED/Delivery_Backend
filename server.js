7bda6aa4c1ae3d8a5657d9ca6478c3358c89e8d4
MD SIFAT AHAMED <mdsifat978@gmail.com>
2025-12-06 01:24:36 +0600

    Fix: cros related error

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ const app = express();
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
 
 // Test Route
 app.get("/", (req, res) => {
